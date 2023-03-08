a306aa94e6de608bf2a768923ebb0e3b5c601612
DESKTOP-JBJR9JL\jongseoung <jjong015189@naver.com>
2023-03-08 15:10:05 +0900

    배열과 문자열 연습문제

---
+++ C/Training/PracticeProblem_Arrary.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<time.h>
+
+int main(void) {
+	
+	srand(time(NULL));
+	printf("==== 아빠는 대머리 게임 ====\n");
+	int answer; // 사용자 입력값
+	int treatment = rand() % 4; // 발모제 선택
+	
+	int cntShowBottle = 0; // 이번 게임에 보여줄 병 갯수
+	int prevCntShowBottle = 0; // 앞 게임에 보여준 병 갯수
+
+	// 3번의 기회
+	for (int i = 1; i <= 3; i++) {
+		int bottle[4] = { 0,0,0,0 };
+		do {
+			cntShowBottle = rand() % 2 + 2; //보여줄 병의 갯수
+		} while (cntShowBottle == prevCntShowBottle);
+		prevCntShowBottle = cntShowBottle;
+		int isincluded = 0; // 보여줄 병중에 발모제가 포함되어 있는지 여부
+
+		printf(" >> %d 번째 시도 : ", i);
+
+		// 보여줄 병 종류 선택
+		for (int j=0; j < cntShowBottle; j++) {
+			int randBottle = rand() % 4;
+
+			if (bottle[randBottle] == 0) {
+				bottle[randBottle] = 1;
+				if (randBottle == treatment) {
+					isincluded = 1;
+				}
+			}
+			else {
+				j--;
+			}
+		}
+
+		// 사용자에게 표시
+		for (int k = 0; k < 4; k++) {
+			if (bottle[k] == 1) {
+				printf("%d ", k + 1);
+			}
+		}
+		printf("물약을 머리에 바릅니다. \n");
+
+		if (isincluded == 1) {
+			printf(" >> 성공 ! 머리가 났어요!! \n");
+		}
+		else {
+			printf(" >> 실패 ! 머리가 나지않았어요..ㅠㅠ \n");
+		}
+		printf("계속하려면 아무키나 누르세요. \n");
+		getchar();
+	}
+
+	printf("\n\n발모제는 몇번일까요?");
+	scanf_s("%d", &answer);
+
+	if (answer == treatment+1) {
+		printf(" >> 정답입니다.");
+	}
+	else {
+		printf(" >> 오답입니다. 정답은 %d 입니다 \n",treatment+1);
+	}
+	return 0;
+}
